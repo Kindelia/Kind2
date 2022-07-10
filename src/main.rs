e45347a3d52115842e6c2867dce4ab9d5b5a07fd
Victor Maia <victor.taelin@gmail.com>
2022-07-10 08:46:31 -0300

    Initial compilation to HVM

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,45 @@
 #![allow(unused_variables, dead_code)]
 
-mod cli;
-mod vm;
+pub use clap::Parser;
+use clap::Subcommand;
 
-use cli::{Parser, Cli};
-
+const KI: usize = 1024;
+const MI: usize = 1024 * 1024;
+const GI: usize = 1024 * 1024 * 1024;
 const KIND2_HVM_CODE: &str = include_str!("kind2.hvm");
+
+#[derive(Parser)]
+#[clap(author, version, about, long_about = None)]
+#[clap(propagate_version = true)]
+pub struct Cli {
+  #[clap(subcommand)]
+  pub command: CliCmd,
+}
+
+#[derive(Subcommand)]
+pub enum CliCmd {
+  /// Runs the Main term of a Kind2 file
+  Run { 
+    /// Input file
+    file: String,
+    #[clap(short, long)]
+    debug: bool,
+  },
+
+  /// Checks a Kind2 file
+  Check { 
+    /// Input file
+    file: String,
+    #[clap(short, long)]
+    debug: bool,
+  },
+
+  /// Compile a Kind2 file to HVM
+  Compile {
+    /// Input file
+    file: String,
+  },
+}
 
 fn main() {
   match run() {
@@ -17,44 +51,92 @@ fn main() {
   }
 }
 
-const KI: usize = 1024;
-const MI: usize = 1024 * 1024;
-const GI: usize = 1024 * 1024 * 1024;
+pub fn build_str_term(txt: &str) -> hvm::language::Term {
+  use hvm::language::Term;
+  let empty = Term::Ctr { name: "StrNil".to_string(), args: Vec::new() };
+  let list = txt.chars().rfold(empty, |t, c| Term::Ctr {
+    name: "StrCons".to_string(),
+    args: vec![Box::new(Term::Num { numb: c as u64 }), Box::new(t)],
+  });
+  list
+}
 
 fn run() -> Result<(), String> {
   let cli_matches = Cli::parse();
 
+  // reads the local file "kind2.hvm" to string, using fs:
+  //let mut kind2_hvm_path = std::env::current_exe().unwrap();
+  //kind2_hvm_path.pop();
+  //kind2_hvm_path.push("kind2.hvm");
+  //println!("-> {:?}", kind2_hvm_path);
+  //let kind2_hvm_code = load_file(&String::from(kind2_hvm_path.to_string_lossy()))?;
+
   match cli_matches.command {
-    //cli::CliCmd::Compile{ file } => {
-      //let code = load_file(&file)?;
-      //Err("Compiling not implemented".to_string())
-    //}
-    
-    cli::CliCmd::Run{ file, debug } => {
+    // TODO: avoid repetition
+
+    CliCmd::Run{ file, debug } => {
       let code_str = load_file(&file)?;
 
       // Grows the stack. Without this, the code overflows sometimes on debug
       // mode. TODO: Investigate.
       stacker::grow(
         64 * MI, 
-        || do_the_thing(KIND2_HVM_CODE, "Kind2.Run", &code_str)
+        || do_the_thing(&KIND2_HVM_CODE, "Kind2.Run", &code_str)
       )?;
       Ok(())
     }
 
-    cli::CliCmd::Check{ file, debug } => {
+    CliCmd::Check{ file, debug } => {
       let code_str = load_file(&file)?;
 
       // Grows the stack. Without this, the code overflows sometimes on debug
       // mode. TODO: Investigate.
       stacker::grow(
         64 * MI, 
-        || do_the_thing(KIND2_HVM_CODE, "Kind2.Check", &code_str)
+        || do_the_thing(&KIND2_HVM_CODE, "Kind2.Check", &code_str)
       )?;
       Ok(())
+    }
+
+    CliCmd::Compile { file } => {
+      let code_str = load_file(&file)?;
+
+      // Grows the stack. Without this, the code overflows sometimes on debug
+      // mode. TODO: Investigate.
+      stacker::grow(
+        64 * MI, 
+        || do_the_thing(&KIND2_HVM_CODE, "Kind2.Compile", &code_str)
+      )?;
+      Ok(())
+
     }
 
   }
+}
+
+fn read_string(rt: &hvm::runtime::Worker, host: u64, str_cons: u64, str_nil: u64, i2n: Option<&std::collections::HashMap<u64, String>>) -> String {
+  let mut term = hvm::runtime::ask_lnk(rt, host);
+  let mut text = String::new();
+  //let str_cons = rt.
+  loop {
+    if hvm::runtime::get_tag(term) == hvm::runtime::CTR {
+      let fid = hvm::runtime::get_ext(term);
+      if fid == str_cons {
+        let head = hvm::runtime::ask_arg(rt, term, 0);
+        let tail = hvm::runtime::ask_arg(rt, term, 1);
+        if hvm::runtime::get_tag(head) == hvm::runtime::NUM {
+          text.push(std::char::from_u32(hvm::runtime::get_num(head) as u32).unwrap_or('?'));
+          term = tail;
+          continue;
+        }
+      }
+      if fid == str_nil {
+        break;
+      }
+    }
+    panic!("Invalid output: {} {}", hvm::runtime::get_tag(term), hvm::runtime::show_term(rt, term, i2n, 0));
+  }
+  return text;
 }
 
 // TODO: this will be renamed, eventually
@@ -63,9 +145,7 @@ fn do_the_thing(kind2_code: &str, call_fn_name: &str, call_fn_argm: &str) -> Res
   use hvm::runtime as rt;
   use hvm::rulebook as rb;
   use hvm::builder as bd;
-  use hvm::readback as rd;
-
-  let mut worker = rt::new_worker();
+  //use hvm::readback as rd;
 
   // Parses and reads the input file
   let file = lang::read_file(kind2_code)?;
@@ -73,12 +153,15 @@ fn do_the_thing(kind2_code: &str, call_fn_name: &str, call_fn_argm: &str) -> Res
   // Converts the HVM "file" to a Rulebook
   let book = rb::gen_rulebook(&file);
 
-  // Builds dynamic functions
-  let functions = bd::build_runtime_functions(&book);
+  let str_cons = *book.name_to_id.get("StrCons").unwrap_or(&0);
+  let str_nil  = *book.name_to_id.get("StrNil").unwrap_or(&1);
 
-  let str_term = vm::build_str_term(call_fn_argm);
+  // Builds worker
+  let mut worker = rt::new_worker();
+  worker.funs = bd::build_runtime_functions(&book);
+  worker.aris = bd::build_runtime_arities(&book);
 
-  // dbg!(&str_term);
+  let str_term = build_str_term(call_fn_argm);
 
   let main_call = lang::Term::Ctr {
     name: call_fn_name.to_string(),
@@ -86,25 +169,31 @@ fn do_the_thing(kind2_code: &str, call_fn_name: &str, call_fn_argm: &str) -> Res
   };
   let main_pos = bd::alloc_term(&mut worker, &book, &main_call);
 
-  rt::normal(&mut worker, main_pos, &functions, Some(&book.id_to_name), false);
+  println!("- Reducing.");
+
+  rt::normal(&mut worker, main_pos, Some(&book.id_to_name), false);
+
+  println!("- Reduced. {} rewrites.", worker.cost);
 
   // Reads it back to a HVM string
-  let book = Some(book);
-  let result = match rd::as_term(&worker, &book, main_pos) {
-    Ok(x)   => format!("{}", x),
-    Err(..) => rd::as_code(&worker, &book, main_pos),
-  };
+  //let book = Some(book);
+  //let text = match rd::as_term(&worker, &book, main_pos) {
+    //Ok(x)   => format!("{}", x),
+    //Err(..) => rd::as_code(&worker, &book, main_pos),
+  //};
 
   // FIXME: this should be a proper function that prints HVM strings (StrCons ...)
-  let mut output = format!("{}", result);
-  if &output[0 .. 2] == "\"\"" {
-    output = "\nAll terms check.\n".to_string();
-  }
-  if &output[0 .. 1] == "\"" {
-    output = output[1 .. output.len() - 1].to_string();
-  }
+  //let mut output = format!("{}", text);
+  //if &output[0 .. 2] == "\"\"" {
+    //output = "\nAll terms check.\n".to_string();
+  //}
+  //if &output[0 .. 1] == "\"" {
+    //output = output[1 .. output.len() - 1].to_string();
+  //}
 
-  println!("{}", output);
+  let text = read_string(&worker, main_pos, str_cons, str_nil, Some(&book.id_to_name));
+  println!("");
+  println!("{}", text);
 
   Ok(())
 }
